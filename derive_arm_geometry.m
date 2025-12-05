6eaf60136521ba744c0241fcaf6b323024f37e8e
hben09 <hben09321@gmail.com>
2025-12-04 19:17:50 -0500

    defined symbols for equation of motion

---
+++ derive_arm_geometry.m
@@ -0,0 +1,30 @@
+% Benjamin Hsu
+% ME500 Final Project
+%
+% SCRIPT: derive_arm_equation.m
+% PURPOSE: Symobolically derive the equations of motion for a 3-DOF arm.
+
+clear; clc;
+
+%%% Define variables
+
+% Mass and length of links
+syms a2 a3 m1 m2 m3 g real
+
+% Inertia Matrices
+syms Ixx1 Iyy1 Izz1 Ixx2 Iyy2 Izz2 Ixx3 Iyy3 Izz3 real
+
+% State Variables (Angles and Velocity)
+syms q1 q2 q3 dq1 dq2 dq3 real
+
+% Group into vectors for easy calculation
+q = [q1; q2; q3]; % Position Vector
+dq = [dq1; dq2; dq3]; % Velocity Vector
+
+% Define Interia Matricies relative to COM
+I1 = diag([Ixx1, Iyy1, Izz1])
+I2 = diag([Ixx2, Iyy2, Izz2])
+I3 = diag([Ixx3, Iyy3, Izz3])
+
+
+
